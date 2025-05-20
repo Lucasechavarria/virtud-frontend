ef8f5038534f5b42ced53ddcb6eb2356be89f4b7
Lucas Echavarria <echavarrialucas1986@gmail.com>
2025-05-20 08:33:34 -0300

    Initial commit with Firebase configuration

---
+++ app/_app.tsx
@@ -0,0 +1,14 @@
+﻿'use client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '../lib/react-query';
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
